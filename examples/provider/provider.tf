f521cef49f1b17c8165d4234fa49e8f9a6493afe
Thomas Rooney <thomas@speakeasyapi.dev>
2024-06-06 20:15:51 +0900

    chore: initial generation

---
+++ examples/provider/provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    todoist = {
+      source  = "speakeasy/todoist"
+      version = "0.0.1"
+    }
+  }
+}
+
+provider "todoist" {
+  # Configuration options
+}
